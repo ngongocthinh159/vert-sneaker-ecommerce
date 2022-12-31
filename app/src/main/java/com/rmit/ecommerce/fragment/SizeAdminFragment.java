896d750bda3676cb1b573ecc389c9564ddac6d03
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-12-31 12:59:46 +0700

    feat: add size manager layout

--- app/src/main/java/com/rmit/ecommerce/fragment/SizeAdminFragment.java
+++ app/src/main/java/com/rmit/ecommerce/fragment/SizeAdminFragment.java
@@ -3,25 +3,19 @@ package com.rmit.ecommerce.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-import java.util.ArrayList;
-
-import com.rmit.ecommerce.NotificationRVAdapter;
 import com.rmit.ecommerce.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link NotificationFragment#newInstance} factory method to
+ * Use the {@link SizeAdminFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NotificationFragment extends Fragment {
+public class SizeAdminFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,7 +26,7 @@ public class NotificationFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public NotificationFragment() {
+    public SizeAdminFragment() {
         // Required empty public constructor
     }
 
@@ -42,11 +36,11 @@ public class NotificationFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment NotificationFragment.
+     * @return A new instance of fragment SizeAdminFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static NotificationFragment newInstance(String param1, String param2) {
-        NotificationFragment fragment = new NotificationFragment();
+    public static SizeAdminFragment newInstance(String param1, String param2) {
+        SizeAdminFragment fragment = new SizeAdminFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -66,28 +60,7 @@ public class NotificationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_notification, container, false);
-        Button clearNotiBtn = view.findViewById(R.id.clearNotiBtn);
-        setupRecyclerView(view);
-        clearNotiBtn.setOnClickListener(v -> {
-            // TODO: Clear local notification
-            System.out.println("CLEARED");
-        });
         // Inflate the layout for this fragment
-        return view;
-    }
-
-    private void setupRecyclerView(View view) {
-        RecyclerView notiRV = view.findViewById(R.id.notiRV);
-        ArrayList<String> notifications = new ArrayList<String>();
-        notifications.add("1");
-        notifications.add("2");
-        NotificationRVAdapter notificationRVAdapter = new NotificationRVAdapter(notifications);
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false);
-
-        notiRV.setAdapter(notificationRVAdapter);
-        notiRV.setLayoutManager(layoutManager);
-
+        return inflater.inflate(R.layout.fragment_size_admin, container, false);
     }
 }
